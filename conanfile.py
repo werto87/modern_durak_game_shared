672b4cd37cd82e47f9c30192513341438f0137a6
werto87 <huhulasmichinruhe@web.de>
2024-02-01 15:51:13 +0900

    update to new cmake and conan version2

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,15 @@
-from conans import ConanFile, tools
-from conans.tools import check_min_cppstd
-from conans.errors import ConanInvalidConfiguration, ConanException
-import os
-from conans.tools import Version
+from conan import ConanFile
 
 
-class modern_durak_game_shared(ConanFile):
-    version ="1.0.0"
-    name = "modern_durak_game_shared"
-    homepage = "https://github.com/werto87/modern_durak_game_shared"
-    description = "uses boost::fusion to help with serialization; json <-> user defined type"
-    topics = ("json parse", "serialization", "user defined type")
-    license = "BSL-1.0"
-    url = "https://github.com/conan-io/conan-center-index"
-    settings = "compiler"
-    exports_sources = "modern_durak_game_shared/*"
+class Project(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def configure(self):
+        self.options["catch2"].with_main = True
+        self.options["catch2"].with_benchmark = True
 
     def requirements(self):
-        self.requires("durak/[~0]@werto87/stable")
-        self.requires("confu_json/[~1]")
-
-    def package(self):
-        self.copy("modern_durak_game_shared/modern_durak_game_shared.hxx", dst="include/")
-
-    def package_id(self):
-        self.info.header_only()
+        self.requires("catch2/2.13.9")
+        self.requires("durak/1.0.0")
+        self.requires("confu_json/1.0.1")
