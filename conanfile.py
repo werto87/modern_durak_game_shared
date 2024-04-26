9e29d03b7ddc80a411b476f28bafb82f00148dde
walde <w-schneider1987@web.de>
2024-04-26 16:49:49 +0900

    new cmake update

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,15 @@
 from conan import ConanFile
-
+from conan.tools.cmake import CMakeToolchain
 
 class Project(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators =  "CMakeDeps"
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False #workaround because this leads to useless options in cmake-tools configure
+        tc.generate()
+
 
     def configure(self):
         self.options["catch2"].with_main = True
@@ -13,4 +19,4 @@ class Project(ConanFile):
         self.requires("modern_durak_game_option/latest")
         self.requires("catch2/2.13.9")
         self.requires("durak/1.0.3")
-        self.requires("confu_json/1.0.1")
+        self.requires("confu_json/[>=1.0.2 <2]")        
